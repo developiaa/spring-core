77926e7cad242145d31549f6c58061f8bd74b099
developiaa <developiaa@gmail.com>
2021-07-21 22:46:26 +0900

    feat : Test autowired option

--- src/main/java/developia/core/AutoAppConfig.java
+++ src/main/java/developia/core/AutoAppConfig.java
@@ -17,8 +17,8 @@ public class AutoAppConfig {
     // 자동 빈 등록과 수동 빈 등록이 중복될 경우
     // 수동 빈 등록이 자동 빈 등록을 오버라이딩 한다.
     // >> 이런 부분은 사실 굉장히 잡기 어려운 부분이기 때문에 테스트가 아닌 부분에서 실행할 경우 기본값으로 에러가 나는 부분으로 변경됨
-    @Bean(name="memoryMemberRepository")
-    MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name="memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
